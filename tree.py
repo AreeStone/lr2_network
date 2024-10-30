f73b90de816bb31568fdf4f2ddc4031bf5a372cf
Aleksandr Pavlovsky <areestone12@gmail.com>
2024-10-30 18:06:21 +0300

    init commit

--- tree.py
+++ tree.py
@@ -1,44 +1,62 @@
 from mininet.net import Mininet
-from mininet.node import RemoteController
+from mininet.node import Controller, OVSKernelSwitch, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
+from mininet.log import setLogLevel
+import time
 
 
 def treeTopo():
     net = Mininet(controller=RemoteController)
 
-    info('*** Adding controller\n')
     net.addController('c0')
 
-    info('*** Adding hosts\n')
     h1 = net.addHost('h1', ip='10.0.0.1', mac='00:00:00:00:00:01')
+    h2 = net.addHost('h2', ip='10.0.0.2', mac='00:00:00:00:00:02')
+    h3 = net.addHost('h3', ip='10.0.0.3', mac='00:00:00:00:00:03')
     h4 = net.addHost('h4', ip='10.0.0.4', mac='00:00:00:00:00:04')
-    h5 = net.addHost('h5', ip='10.0.0.5', mac='00:00:00:00:00:05')
-    h6 = net.addHost('h6', ip='10.0.0.6', mac='00:00:00:00:00:06')
 
-    info('*** Adding switches\n')
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
     s3 = net.addSwitch('s3')
     s4 = net.addSwitch('s4')
+    s5 = net.addSwitch('s5')
 
-    info('*** Creating links\n')
     net.addLink(h1, s1)
-    net.addLink(h4, s4)
-    net.addLink(h5, s2)
-    net.addLink(h6, s4)
+    net.addLink(h2, s4)
+    net.addLink(h3, s5)
+    net.addLink(h4, s5)
 
     net.addLink(s1, s2)
-    net.addLink(s2, s3)
-    net.addLink(s3, s4)
+    net.addLink(s1, s5)
 
-    info('*** Starting network\n')
+    net.addLink(s2, s3)
+
+    net.addLink(s3, s4)
+    net.addLink(s3, s5)
+
+    net.addLink(s4, s5)
+
     net.start()
 
-    info('*** Running CLI\n')
-    CLI(net)
+    s2.cmd('ovs-vsctl set Bridge s2 other_config:stp-priority=0x7000')  # s2 станет корневым мостом
+    s1.cmd('ovs-vsctl set Bridge s1 other_config:stp-priority=0x7800')
+    s3.cmd('ovs-vsctl set Bridge s3 other_config:stp-priority=0x8000')
+    s4.cmd('ovs-vsctl set Bridge s4 other_config:stp-priority=0x8800')
+    s5.cmd('ovs-vsctl set Bridge s5 other_config:stp-priority=0x9000')
 
-    info('*** Stopping network')
+    s1.cmd('ovs-vsctl set bridge s1 stp_enable=true')
+    s2.cmd('ovs-vsctl set bridge s2 stp_enable=true')
+    s3.cmd('ovs-vsctl set bridge s3 stp_enable=true')
+    s4.cmd('ovs-vsctl set bridge s4 stp_enable=true')
+    s5.cmd('ovs-vsctl set bridge s5 stp_enable=true')
+
+    s1.cmdPrint('ovs-ofctl dump-ports-desc s1')
+    s2.cmdPrint('ovs-ofctl dump-ports-desc s2')
+    s3.cmdPrint('ovs-ofctl dump-ports-desc s3')
+    s4.cmdPrint('ovs-ofctl dump-ports-desc s4')
+    s5.cmdPrint('ovs-ofctl dump-ports-desc s5')
+
+    CLI(net)
     net.stop()
 
 
